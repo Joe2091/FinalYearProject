17b7b0d4908bd5026133fdc50662395dca609a3b
Joe <x21476132@student.ncirl.ie>
2025-03-24 12:47:37 +0000

    Connection Between Frontend and EC2 instance fix

--- Backend/server.js
+++ Backend/server.js
@@ -106,6 +106,6 @@ app.delete("/api/notes/:id", async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
