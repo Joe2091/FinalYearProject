001b273a961f8e54098d5c98a67d82df56646838
Joe <x21476132@student.ncirl.ie>
2025-04-20 02:08:29 +0100

    added Pinia skeleton

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
@@ -7,4 +8,4 @@ import router from './router';
 
 import '@mdi/font/css/materialdesignicons.css';
 
-createApp(App).use(vuetify).use(router).mount('#app');
+createApp(App).use(vuetify).use(router).use(createPinia).mount('#app');
