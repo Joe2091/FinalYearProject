9cbecd80de07f607c69e1a9662f31901366ac278
Joe <x21476132@student.ncirl.ie>
2025-04-22 18:21:11 +0100

    synced users from Firebase with MongoDB, users can only see and alter their own notes.

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
+  createdBy: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Note", NoteSchema);
+module.exports = mongoose.model('Note', NoteSchema);
